7e207b39b551c561da8c7096bfb68ab8a0c4e0ac
MiisterNarsikBis <48716250+MiisterNarsikBis@users.noreply.github.com>
2023-07-14 17:27:26 +0200

    patch ?

--- index.js
+++ index.js
@@ -58,81 +58,49 @@ app.get('/gpt/:text', async (req, res) => {
     let maxLength = 399; // Longueur maximale souhaitée
 
 
-    if (GPT_MODE === "CHAT"){
-      //CHAT MODE EXECUTION
+    //CHAT MODE EXECUTION
 
-      //Add user message to  messages
-      messages.push({role: "user", content: text})
-      //Check if message history is exceeded
-      console.log("Conversations in History: " + ((messages.length / 2) -1) + "/" + process.env.HISTORY_LENGTH)
-      if(messages.length > ((process.env.HISTORY_LENGTH * 2) + 1)) {
-          console.log('Message amount in history exceeded. Removing oldest user and agent messages.')
-          messages.splice(1,2)
-     }
-    
-      console.log("Messages: ")
-      console.dir(messages)
-      console.log("User Input: " + text)
-
-      const response = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages: messages,
-        temperature: 0.5,
-        max_tokens: 100,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0,
-      });
-    
-      if (response.data.choices) {
-        let agent_response = response.data.choices[0].message.content
-
-        console.log ("Agent answer: " + agent_response)
-        messages.push({role: "assistant", content: agent_response})
-
-        let regex = /[.!?](?=[^.!?]*$)/;
-        let dernierIndex = agent_response.search(regex);
-
-        agent_response = agent_response.substring(0, (dernierIndex +1))
-        console.log("Sliced Agent answer: " + agent_response);
-        
-        res.send(agent_response)
-      } else {
-        res.send("Something went wrong. Try again later!")
-      }
-
-    } else {
-      //PROMPT MODE EXECUTION
-      const prompt = file_context + "\n\nQ:" + text + "\nA:";
-      console.log("User Input: " + text)
-
-      const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: prompt,
-        temperature: 0.5,
-        max_tokens: 100,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0,
-      });
-      if (response.data.choices) {
-        let agent_response = response.data.choices[0].text
-        let agent_response = response.data.choices[0].message.content
-
-        console.log ("Agent answer: " + agent_response)
-        messages.push({role: "assistant", content: agent_response})
-
-        let regex = /[.!?](?=[^.!?]*$)/;
-        let dernierIndex = agent_response.search(regex);
-
-        agent_response = agent_response.substring(0, (dernierIndex +1))
-        console.log("Sliced Agent answer: " + agent_response);
-        
-        res.send(agent_response)
-      } else {
-          res.send("Something went wrong. Try again later!")
-      }
+    //Add user message to  messages
+    messages.push({role: "user", content: text})
+    //Check if message history is exceeded
+    console.log("Conversations in History: " + ((messages.length / 2) -1) + "/" + process.env.HISTORY_LENGTH)
+    if(messages.length > ((process.env.HISTORY_LENGTH * 2) + 1)) {
+        console.log('Message amount in history exceeded. Removing oldest user and agent messages.')
+        messages.splice(1,2)
     }
+  
+    console.log("Messages: ")
+    console.dir(messages)
+    console.log("User Input: " + text)
+
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: messages,
+      temperature: 0.5,
+      max_tokens: 100,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+    });
+  
+    if (response.data.choices) {
+      let agent_response = response.data.choices[0].message.content
+
+      console.log ("Agent answer: " + agent_response)
+      messages.push({role: "assistant", content: agent_response})
+
+      let regex = /[.!?](?=[^.!?]*$)/;
+      let dernierIndex = agent_response.search(regex);
+
+      agent_response = agent_response.substring(0, (dernierIndex +1))
+      console.log("Sliced Agent answer: " + agent_response);
+      
+      res.send(agent_response)
+    } else {
+      res.send("Something went wrong. Try again later!")
+    }
+
+    
     
 })
 app.listen(process.env.PORT || 3000)
